993679112b8f1d6174a5fc356d06b43f6ae08063
Giovanni <giovanniw30@hotmail.com>
2024-04-10 16:38:21 -0300

    create multiple pages

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    return render(request, 'homepage.html')
+
+def store(request):
+    return render(request, 'store.html')
+
+def my_account(request):
+    return render(request, 'my_account.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def cart(request):
+    return render(request, 'cart.html')
+
+def checkout(request):
+    return render(request, 'checkout.html')
